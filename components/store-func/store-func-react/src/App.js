5ce97b10603f0ec5e7e35e21640c78b945100208
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2021-01-17 00:48:12 +0300

    add html

---
+++ components/store-func/store-func-react/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import ShopItemFunc from './components/ShopItemFunc';
+import React from 'react';
+
+
+
+function App() {
+
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+  }
+
+  return <ShopItemFunc item={item}></ShopItemFunc>
+}
+
+export default App;
---
+++ components/store-func/store-func-react/src/components/ShopItemFunc.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ItemModel from '../models/ItemSample';
+
+function ShopItemFunc(props) {
+
+    const { item } = props;
+
+      return (
+        <div className="main-content">
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div className='description'>
+                {item.descriptionFull}
+            </div>
+            <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+            <div className="diliver"></div>
+            <div className="purchase-info">
+                <div className="price">{item.price}{item.currency}</div>
+                <button>Add to shopping cart</button>
+            </div>
+        </div>
+      );
+}
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.instanceOf(ItemModel).isRequired
+}
+
+export default ShopItemFunc;
