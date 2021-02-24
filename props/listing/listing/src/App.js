ab4c8a51a0b34ee1fe9a438f6c806366f4dbe0a7
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2021-02-24 18:08:47 +0300

    add details in ListItem/item-details

--- props/listing/listing/src/App.js
+++ props/listing/listing/src/App.js
@@ -1,26 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import etsy from './models/data'
+import Listing from './models/Listing'
 
 function App() {
   console.log(etsy)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Listing items={etsy}></Listing>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
--- props/listing/listing/src/models/ListItem.js
+++ props/listing/listing/src/models/ListItem.js
@@ -2,18 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function ListItem({elementItem}) {
-    const {url, title} = elementItem
+    const {url, title, MainImage, item, quantity} = elementItem
+
+    function getTitle(title) {
+        return title.length > 50 ? title.slice(0, 50) + '…' : title;
+      }
+
     return (
         <div className="item">
             <div className='item-image'>
                 <a href={url}>
-                {/* <img src={MainImage && MainImage.url_570xN} alt=''/> */}
+                <img src={MainImage && MainImage.url_570xN} alt=''/>
                 </a>
             </div>
             <div className="item-details">
-                <p className="item-title">{title}</p>
-                {/* <p className="item-price">{item.currency_code}{item.price}</p> */}
-                <p className="item-quantity level-medium">10</p>
+                <p className="item-title">{title && getTitle(title)}</p>
+                <p className="item-price">{item.currency_code}{item.price}</p>
+                <p className={`item-quantity level-${quantity && quantity > 20 ? 'high' : quantity > 10 ? 'medium' : 'low'}`}>{`${quantity} left`}</p>
             </div>
         </div>
     )
