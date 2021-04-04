0cb493ec9f1d2826d37866d7664b27d5df4827d9
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2021-04-04 04:37:07 +0300

    add list and details

--- hooks-context/use-effect/use_effect/src/App.js
+++ hooks-context/use-effect/use_effect/src/App.js
@@ -1,114 +1,27 @@
 import React, { useState } from 'react';
+import List from './components/List/List';
+import './App.css';
+import useJsonFetch from './hooks/useJsonFetch';
+import Details from './components/Details/Details';
 
-function New(props) {
-    return (
-        <div className="wrap-item wrap-item-new">
-            <span className="label">New!</span>
-            {props.children}
-        </div>
-    )
-};
+function App() {
+  const [users, setUsers] = useState([]);
+  const [selectedUserID, setSelectedUserID] = useState(null);
 
-function Popular(props) {
-    return (
-        <div className="wrap-item wrap-item-popular">
-            <span className="label">Popular!</span>
-            {props.children}
-        </div>
-    )
-};
+  const [{ data, loading }] = useJsonFetch(`${process.env.REACT_APP_USERS_URL}/users.json`);
+  if (data && (JSON.stringify(users) !== JSON.stringify(data))) setUsers(data);
 
-function Article(props) {
-    return (
-        <div className="item item-article">
-            <h3><a href="#">{props.title}</a></h3>
-            <p className="views">Прочтений: {props.views}</p>
-        </div>
-    )
-};
-
-function Video(props) {
-    return (
-        <div className="item item-video">
-            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <p className="views">Просмотров: {props.views}</p>
-        </div>
-    )
-};
-
-
-function  SortType(views) {
-    const type = (views < 1000) ? 'New' : 'Popular'
-
-    console.log(type);
-    return type
+  return (
+    <div className='App'>
+      { loading && <div>{'Loading...'}</div> }
+      <List
+        items={users}
+        selectedID={selectedUserID}
+        onClick={(id) => setSelectedUserID(id)}
+      />
+      { selectedUserID && <Details info={users.find((o) => o.id === selectedUserID)}/> }
+    </div>
+  );
 }
 
-function SortHighlight(Component) {
-    return class extends React.Component {
-        render() {
-            console.log(this.props.views);
-            const TypeActual = SortType(this.props.views)
-            return <TypeActual><Component {...this.props}></Component></TypeActual>
-        }
-    }
-} 
-
-const ArticleHighlight = SortHighlight(Article);
-const VideoHighlight = SortHighlight(Video);
-
-
-function List(props) {
-    return props.list.map(item => {
-        switch (item.type) {
-            case 'video':
-                return (
-                    <VideoHighlight {...item} />
-                );
-
-            case 'article':
-                return (
-                    <ArticleHighlight {...item} />
-                );
-        }
-    });
-};
-
-export default function App() {
-    const [list, setList] = useState([
-        {
-            type: 'video',
-            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
-            views: 50
-        },
-        {
-            type: 'video',
-            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
-            views: 12
-        },
-        {
-            type: 'article',
-            title: 'Невероятные события в неизвестном поселке...',
-            views: 175
-        },
-        {
-            type: 'article',
-            title: 'Секретные данные были раскрыты!',
-            views: 1532
-        },
-        {
-            type: 'video',
-            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
-            views: 4253
-        },
-        {
-            type: 'article',
-            title: 'Кот Бегемот обладает невероятной...',
-            views: 12,
-        },
-    ]);
-
-    return (
-        <List list={list} />
-    );
-}
+export default App;
